512eeacc8ab4fd9ef2bf0b0fb0e95273150ae44d
wagnojunior <wagnojunior@gmail.com>
2023-03-10 09:38:43 +0900

    Added tests
    - get task by tag
    - get task by due date

--- internal/taskstore/handlers_test.go
+++ internal/taskstore/handlers_test.go
@@ -43,7 +43,7 @@ func TestCreateTaskHandler(t *testing.T) {
 	// Checks the HTTP reponse status code
 	status := w.Code
 	if status != http.StatusOK {
-		t.Errorf("handler returned the wrong status code: got %v want %v", status, http.StatusOK)
+		t.Errorf("handler returned the wrong status code: got %v wanted %v", status, http.StatusOK)
 	}
 }
 
@@ -52,21 +52,18 @@ func TestGetAllTasksHandler(t *testing.T) {
 }
 
 func TestGetTaskByTagHandler(t *testing.T) {
-	// Creates a test task and marshals it into a JSON format
+	// Creates test task and a task server
 	task := Task{
-		ID:   1,
+		ID:   0,
 		Text: "First task",
-		Tags: []string{"Tag_1", "Tag_2"},
+		Tags: []string{"tag_1", "tag_2"},
 		Due:  time.Now(),
 	}
-	taskJSON, err := json.Marshal(task)
-	if err != nil {
-		t.Fatal(err)
-	}
+	ts := NewTaskServer()
+	_ = ts.store.CreateTask(task.Text, task.Tags, task.Due)
 
-	// Creates a GET request to the `/tag/` directory and with `taskJSON`
-	// as body
-	r, err := http.NewRequest("GET", "/tag/tag_1", bytes.NewBuffer(taskJSON))
+	// Creates a GET request to the `/tag/` directory
+	r, err := http.NewRequest("GET", "/tag/tag_1", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -75,16 +72,55 @@ func TestGetTaskByTagHandler(t *testing.T) {
 	// `http.ResponseWriter`
 	w := httptest.NewRecorder()
 
-	// Crestes a task server
-	ts := NewTaskServer()
-
 	// Serves HTTP
 	handler := http.HandlerFunc(ts.getTaskByTagHandler)
 	handler.ServeHTTP(w, r)
 
-	// Checks the HTTP reponse status code
-	status := w.Code
-	if status != http.StatusOK {
-		t.Errorf("handler returned the wrong status code: got %v want %v", status, http.StatusOK)
+	// Gets the task from the response writer and unmarshal it into a struct
+	var got []Task
+	err = json.Unmarshal(w.Body.Bytes(), &got)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !compareTask(task, got[0]) {
+		t.Errorf("handler returned the wrong task: got %v wanted %v", got[0], task)
+	}
+}
+
+func TestGetTaskByDueDateHandler(t *testing.T) {
+	// Creates test task and a task server
+	task := Task{
+		ID:   0,
+		Text: "Test task",
+		Tags: []string{"tag_1", "tag_2"},
+		Due:  time.Date(2023, time.September, 5, 0, 0, 0, 0, time.UTC),
+	}
+	ts := NewTaskServer()
+	_ = ts.store.CreateTask(task.Text, task.Tags, task.Due)
+
+	// Creates a GET request to the `/tag/` directory
+	r, err := http.NewRequest("GET", "/due/2023/9/5", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Creates a ResponseRecorder, which is an implementation of
+	// `http.ResponseWriter`
+	w := httptest.NewRecorder()
+
+	// Serves HTTP
+	handler := http.HandlerFunc(ts.getTaskByDueDateHandler)
+	handler.ServeHTTP(w, r)
+
+	// Gets the task from the response writer and unmarshal it into a struct
+	var got []Task
+	err = json.Unmarshal(w.Body.Bytes(), &got)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !compareTask(task, got[0]) {
+		t.Errorf("handler returned the wrong task: got %v wanted %v", got[0], task)
 	}
 }
