f8c74faf7fd4abd50091388b94e0663b9af188e4
wagnojunior <wagnojunior@gmail.com>
2023-03-12 20:08:49 +0900

    Added router
    - TagHandler
    - DueHandler

--- main.go
+++ main.go
@@ -18,6 +18,8 @@ func main() {
 	// Define mux and routes
 	mux := http.NewServeMux()
 	mux.HandleFunc("/task/", server.TaskHandler)
+	mux.HandleFunc("/tag/", server.TagHandler)
+	mux.HandleFunc("/due/", server.DueHandler)
 
 	fmt.Println("Starting server at port :3030...")
 	log.Fatal(http.ListenAndServe("localhost:3030", mux))
