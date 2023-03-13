bc2374b418bdf9f44526b536271fce23cc03daff
wagnojunior <wagnojunior@gmail.com>
2023-03-13 19:42:36 +0900

    Creates utils package
    - DaysIn
    - RenderJSON

---
+++ internal/utils/utils.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+// daysIn calculates and returns how many days in a month for a given year
+func DaysIn(m time.Month, year int) int {
+	return time.Date(year, m+1, 0, 0, 0, 0, 0, time.UTC).Day()
+}
+
+// renderJSON renders `v` as a JSON and writes it in a response writer
+func RenderJSON(w http.ResponseWriter, v any) {
+	json, err := json.Marshal(v)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(json)
+}
