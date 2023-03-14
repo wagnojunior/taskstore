decd22e1bd60d3bc183595f9ef1280cdf78bcd96
wagnojunior <wagnojunior@gmail.com>
2023-03-14 23:30:33 +0900

    Update handlers
    - deleted unnecessary handler
    - use mux.Vars to get URL variables

--- internal/taskstore/handlers.go
+++ internal/taskstore/handlers.go
@@ -2,13 +2,12 @@ package taskstore
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"strconv"
-	"strings"
 	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/wagnojunior/taskstore/internal/utils"
 )
 
@@ -24,53 +23,8 @@ func NewTaskServer() *taskServer {
 	}
 }
 
-// /////////////////////////////////////////////////////////////////////////////
-// TASK HANDLERS
-// /////////////////////////////////////////////////////////////////////////////
-
-func (ts *taskServer) TaskHandler(w http.ResponseWriter, r *http.Request) {
-	// Checks if the request has an ID associated with if
-	if r.URL.Path == "/task/" { // Request without an ID
-		// Checks the requet type
-		if r.Method == http.MethodPost { // Creates a new task
-			ts.createTaskHandler(w, r)
-		} else if r.Method == http.MethodGet { // Gets all tasks
-			ts.getAllTasksHandler(w, r)
-		} else if r.Method == http.MethodDelete { // Deletes all tasks
-			ts.deleteAllTasksHandler(w, r)
-		} else { //
-			http.Error(w, fmt.Sprintf("expect method POST, GET, DELETE but got %v", r.Method), http.StatusMethodNotAllowed)
-			return
-		}
-	} else { // Request with an ID
-		path := strings.Trim(r.URL.Path, "/")
-		subPath := strings.Split(path, "/")
-		if len(subPath) < 2 {
-			http.Error(w, "expected /task/<id> in task handler",
-				http.StatusBadRequest)
-		}
-
-		// Gets the ID
-		id, err := strconv.Atoi(subPath[1])
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		// Checks the method
-		if r.Method == http.MethodDelete { // Deletes a task
-			ts.deleteTaskHandler(w, r, id)
-		} else if r.Method == http.MethodGet { // Gets a task
-			ts.getTaskHandler(w, r, id)
-		} else {
-			http.Error(w, fmt.Sprintf("expect method POST, GET, DELETE but got %v", r.Method), http.StatusMethodNotAllowed)
-			return
-		}
-	}
-}
-
 // `createTaskHandler` handles the creation of new tasks
-func (ts *taskServer) createTaskHandler(w http.ResponseWriter, r *http.Request) {
+func (ts *taskServer) CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("createTaskHandler at %s\n", r.URL.Path)
 
 	// Creates a new decoder and disallow unknowm fields
@@ -92,7 +46,7 @@ func (ts *taskServer) createTaskHandler(w http.ResponseWriter, r *http.Request) 
 }
 
 // getAllTasksHandler handles the getting all tasks
-func (ts *taskServer) getAllTasksHandler(w http.ResponseWriter, r *http.Request) {
+func (ts *taskServer) GetAllTasksHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("getAllTasksHandler at %s\n", r.URL.Path)
 
 	// Gets all the tasks and marshals it to JSON
@@ -101,8 +55,16 @@ func (ts *taskServer) getAllTasksHandler(w http.ResponseWriter, r *http.Request)
 }
 
 // getTaskHandler handles the getting of a task
-func (ts *taskServer) getTaskHandler(w http.ResponseWriter, r *http.Request, id int) {
+func (ts *taskServer) GetTaskHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("getTaskHandler at %s\n", r.URL.Path)
+
+	// Gets a `map[string]string` associated with the http resquest `r`, more specifically the value
+	// associated with the key `id`. Then, converts from str to int
+	id, err := strconv.Atoi(mux.Vars(r)["id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
 
 	// Gets the tasks by ID and marshals it to JSON
 	task, err := ts.store.GetTask(id)
@@ -114,11 +76,19 @@ func (ts *taskServer) getTaskHandler(w http.ResponseWriter, r *http.Request, id 
 }
 
 // deleteTaskHandler handles the deleting of a task
-func (ts *taskServer) deleteTaskHandler(w http.ResponseWriter, r *http.Request, id int) {
+func (ts *taskServer) DeleteTaskHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("deleteTaskHandler at %s\n", r.URL.Path)
 
+	// Gets a `map[string]string` associated with the http resquest `r`, more specifically the value
+	// associated with the key `id`. Then, converts from str to int
+	id, err := strconv.Atoi(mux.Vars(r)["id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
 	// Deletes the task by ID
-	err := ts.store.DeleteTask(id)
+	err = ts.store.DeleteTask(id)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -126,7 +96,7 @@ func (ts *taskServer) deleteTaskHandler(w http.ResponseWriter, r *http.Request, 
 }
 
 // deleteAllTasksHandler handles the deleting of all tasks
-func (ts *taskServer) deleteAllTasksHandler(w http.ResponseWriter, r *http.Request) {
+func (ts *taskServer) DeleteAllTasksHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("deleteAllTasksHandler at %s\n", r.URL.Path)
 
 	// Deletes all tasks
@@ -142,28 +112,12 @@ func (ts *taskServer) deleteAllTasksHandler(w http.ResponseWriter, r *http.Reque
 // /////////////////////////////////////////////////////////////////////////////////////////////////
 
 // TagHandler handles the getting of a task by tag
-func (ts *taskServer) TagHandler(w http.ResponseWriter, r *http.Request) {
+func (ts *taskServer) GetTaskByTagHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("TagHandler at %s\n", r.URL.Path)
 
-	// Checks if requet method is a GET
-	if r.Method != http.MethodGet {
-		http.Error(
-			w,
-			fmt.Sprintf("expected method GET /tag/<tag>, got %s", r.Method),
-			http.StatusMethodNotAllowed)
-	}
-
-	// Get the `tag` from the URL
-	path := strings.Trim(r.URL.Path, "/")
-	subPath := strings.Split(path, "/")
-	println(path, subPath)
-
-	// Check if there are at least one tag in the path. If length of `subPath`
-	// is less than 2, then there are no tags in the path
-	if len(subPath) < 2 {
-		http.Error(w, "expected a path with a least one tag, got none", http.StatusBadRequest)
-	}
-	tag := subPath[1]
+	// Gets a `map[string]string` associated with the http resquest `r`, more specifically the value
+	// associated with the key `tag`
+	tag := mux.Vars(r)["tag"]
 
 	// Get the task by tag and marshal it into JSON
 	tasks := ts.store.GetTaskByTag(tag)
@@ -175,43 +129,23 @@ func (ts *taskServer) TagHandler(w http.ResponseWriter, r *http.Request) {
 // /////////////////////////////////////////////////////////////////////////////////////////////////
 
 // DueHandler handles the getting of a task by due date
-func (ts *taskServer) DueHandler(w http.ResponseWriter, r *http.Request) {
+func (ts *taskServer) GetTaskByDueHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("getTaskByDueDateHandler at %s\n", r.URL.Path)
 
-	// Checks if requet method is a GET
-	if r.Method != http.MethodGet {
-		http.Error(
-			w,
-			fmt.Sprintf("expected method GET /due/<year>/<month>/<day>, got %s", r.Method),
-			http.StatusMethodNotAllowed)
-	}
-
-	// Gets the `tag` from the URL
-	path := strings.Trim(r.URL.Path, "/")
-	subPath := strings.Split(path, "/")
-
-	// Checks if there are at least one tag in the path. If length of `subPath`
-	// is less than 4, then there are missing fields
-	if len(subPath) < 4 {
-		http.Error(w, "expected a path with format /due/<year>/<month>/<day>",
-			http.StatusBadRequest)
-	}
-
-	// Checks the validity of each field in the path
-	year, err := strconv.Atoi(subPath[1])
+	// Gets a `map[string]string` associated with the http resquest `r`
+	vars := mux.Vars(r)
+	year, err := strconv.Atoi(vars["year"])
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
-	month, err := strconv.Atoi(subPath[2])
+	month, err := strconv.Atoi(vars["month"])
 	if err != nil || month < int(time.January) || month > int(time.December) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
-	day, err := strconv.Atoi(subPath[3])
-	if err != nil || day > utils.DaysIn(time.Month(month), year) {
+	day, err := strconv.Atoi(vars["day"])
+	if err != nil || day > utils.DaysIn(month, year) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
