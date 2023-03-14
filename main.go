b37b56f058633d70a14d6771a7df03f54e2ac013
wagnojunior <wagnojunior@gmail.com>
2023-03-14 23:34:18 +0900

    Implement gorilla mux

--- main.go
+++ main.go
@@ -8,19 +8,28 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/wagnojunior/taskstore/internal/taskstore"
 )
 
 func main() {
-	// Starts the taskstore server
+	// Starts Gorilla mux
+	r := mux.NewRouter()
+	r.StrictSlash(true)
+
+	// Starts the task server
 	server := taskstore.NewTaskServer()
 
-	// Define mux and routes
-	mux := http.NewServeMux()
-	mux.HandleFunc("/task/", server.TaskHandler)
-	mux.HandleFunc("/tag/", server.TagHandler)
-	mux.HandleFunc("/due/", server.DueHandler)
+	// Defines the routes
+	r.HandleFunc("/task/", server.CreateTaskHandler).Methods(http.MethodPost)
+	r.HandleFunc("/task/", server.GetAllTasksHandler).Methods(http.MethodGet)
+	r.HandleFunc("/task/", server.DeleteAllTasksHandler).Methods(http.MethodDelete)
+	r.HandleFunc("/task/{id:[0-9]+}/", server.GetTaskHandler).Methods(http.MethodGet)
+	r.HandleFunc("/task/{id:[0-9]+}/", server.DeleteTaskHandler).Methods(http.MethodGet)
+	r.HandleFunc("/tag/{tag}/", server.GetTaskByTagHandler).Methods(http.MethodGet)
+	r.HandleFunc("/due/{year:[0-9]+}/{month:[0-9]+}/{day:[0-9]}/", server.GetTaskByDueHandler).
+		Methods(http.MethodGet)
 
 	fmt.Println("Starting server at port :3030...")
-	log.Fatal(http.ListenAndServe("localhost:3030", mux))
+	log.Fatal(http.ListenAndServe("localhost:3030", r))
 }
