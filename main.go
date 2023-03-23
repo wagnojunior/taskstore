ac65370a39dc0689c68178d25c0d1401580a5d6b
wagnojunior <wagnojunior@gmail.com>
2023-03-23 23:24:50 +0900

    Updated main.go accordingly
    - initialized models, services, and handlers

--- main.go
+++ main.go
@@ -11,12 +11,14 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+	"github.com/wagnojunior/taskstore/internal/handlers"
+	"github.com/wagnojunior/taskstore/internal/models"
 	"github.com/wagnojunior/taskstore/internal/taskstore"
 )
 
 // `appConfig` configures all env variables required for this application to run
 type appConfig struct {
-	PSQL   taskstore.PostgresConfig
+	PSQL   models.PostgresConfig
 	Server struct {
 		Address string
 	}
@@ -57,29 +59,37 @@ func main() {
 	}
 
 	// Opens a connection to the databrase
-	db, err := taskstore.Open(appCfg.PSQL)
+	db, err := models.Open(appCfg.PSQL)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
+	// Creates the necessary services
+	storeService := &models.StoreService{
+		DB: db,
+	}
+	taskService := &models.TaskService{
+		DB: db,
+	}
+
+	// Creates the necessary handlers
+	storeHandler := &handlers.Store{
+		StoreService: storeService,
+	}
+	taskHanler := &handlers.Tasks{
+		TaskService: taskService,
+	}
+
 	// Starts Gorilla mux
 	r := mux.NewRouter()
 	r.StrictSlash(true)
 
-	// Starts the task server
-	server := taskstore.NewTaskServer()
-
-	// Defines the routes
-	r.HandleFunc("/task/", server.CreateTaskHandler).Methods(http.MethodPost)
-	r.HandleFunc("/task/", server.GetAllTasksHandler).Methods(http.MethodGet)
-	r.HandleFunc("/task/", server.DeleteAllTasksHandler).Methods(http.MethodDelete)
-	r.HandleFunc("/task/{id:[0-9]+}/", server.GetTaskHandler).Methods(http.MethodGet)
-	r.HandleFunc("/task/{id:[0-9]+}/", server.DeleteTaskHandler).Methods(http.MethodGet)
-	r.HandleFunc("/tag/{tag}/", server.GetTaskByTagHandler).Methods(http.MethodGet)
-	r.HandleFunc("/due/{year:[0-9]+}/{month:[0-9]+}/{day:[0-9]}/", server.GetTaskByDueHandler).
-		Methods(http.MethodGet)
+	r.HandleFunc("/store", storeHandler.Create).Methods(http.MethodPost)
+	r.HandleFunc("/task", taskHanler.Create).Methods(http.MethodPost)
+	r.HandleFunc("/task/{store_id:[0-9]+}/{id:[0-9]}", taskHanler.GetByID).Methods(http.MethodGet)
 
 	fmt.Println("Starting server at port :3030...")
-	log.Fatal(http.ListenAndServe("localhost:3030", taskstore.LoggingMiddleware(r.ServeHTTP)))
+	log.Fatal(http.ListenAndServe("localhost:3030",
+		taskstore.LoggingMiddleware(r.ServeHTTP)))
 }
