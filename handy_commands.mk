7801fe5af7682ec09b62d789d611419e8d749b2a
wagnojunior <wagnojunior@gmail.com>
2023-03-24 23:36:31 +0900

    Updated handy commands
    - renamed the file
    - added extra commands

---
+++ handy_commands.mk
@@ -0,0 +1,22 @@
+# CURL commands for testing
+
+## POST request to create a new store
+curl -d '{"name":"store_1"}' -H 'Content-Type: application/json' http://localhost:3030/store
+
+## POST request to create a new task
+curl -d '{"store_id":1, "text":"test_task_1", "tags":["test_tag_1"], "due":"2021-02-18"}' -H 'Content-Type: application/json' http://localhost:3030/task
+
+## GET request to get a task by ID
+curl -v http://localhost:3030/task/1/1
+
+
+# Goose commands
+
+## Run migrations
+goose postgres "host=localhost port=5432 user=admin password=admin_taskstore dbname=taskstore sslmode=disable" up
+
+
+# Database
+
+## PSQL
+docker compose exec -it db psql -U admin -d taskstore
