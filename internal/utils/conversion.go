b5c7ca50515ba2f0ae86147dc9e6ff139ceb105e
wagnojunior <wagnojunior@gmail.com>
2023-03-30 18:54:46 +0900

    Fixed typo in comment

--- internal/utils/conversion.go
+++ internal/utils/conversion.go
@@ -12,7 +12,7 @@ func DaysIn(month, year int) int {
 	return time.Date(year, time.Month(month+1), 0, 0, 0, 0, 0, time.UTC).Day()
 }
 
-// renderJSON renders `v` as a JSON and writes it in a response writer
+// `RenderJSON` renders `v` as a JSON and writes it to the response writer
 func RenderJSON(w http.ResponseWriter, v any) {
 	json, err := json.Marshal(v)
 	if err != nil {
