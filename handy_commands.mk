8a92683619b03ce95776cc07d647efdff9bb33c9
wagnojunior <wagnojunior@gmail.com>
2023-03-30 20:45:45 +0900

    Added another handy command

--- handy_commands.mk
+++ handy_commands.mk
@@ -6,6 +6,9 @@ curl -d '{"name":"store_1"}' -H 'Content-Type: application/json' http://localhos
 ## POST request to create a new task
 curl -d '{"store_id":1, "text":"test_task_1", "tags":["test_tag_1"], "due":"2021-02-18"}' -H 'Content-Type: application/json' http://localhost:3030/task
 
+## POST request to delete a task
+curl -d '{"store_id":1, "id":3}' -H 'Content-Type: application/json' http://localhost:3030/delete/1/3
+
 ## GET request to get a task by ID
 curl -v http://localhost:3030/task/1/1
 
