d7c56f0d9419d25e25e37c439a6925effd17feeb
wagnojunior <wagnojunior@gmail.com>
2023-04-09 10:25:57 +0900

    Populated the interface DBRepo with the functions defined in:
    - internal/repository/dbrepo/tasks.go
    - internal/repository/dbrepo/stores.go

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -4,4 +4,12 @@ import "github.com/wagnojunior/taskstore/internal/models"
 
 type DBRepo interface {
 	CreateStore(name string) (*models.Store, error)
+	DeleteStoreByID(id int) error
+
+	CreateTask(storeID int, text string, tags []string, due string) (*models.Task, error)
+	GetTaskByID(storeID, id int) (*models.Task, error)
+	GetTaskByTags(storeID int, tags []string) (*[]models.Task, error)
+	GetAllTasks(storeID int) (*[]models.Task, error)
+	DeleteTaskByID(storeID, id int) error
+	DeleteAllTasks(storeID int) (int, error)
 }
