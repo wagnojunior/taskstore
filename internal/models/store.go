dc864f16ee1a0744ccb981deebcbc1eb27c8a38f
wagnojunior <wagnojunior@gmail.com>
2023-04-09 10:29:43 +0900

    Transferred all functions&methods from:
    - internal/models/store.go
    - internal/models/task.go

--- internal/models/store.go
+++ internal/models/store.go
@@ -4,26 +4,3 @@ type Store struct {
 	ID   int    `json:"id"`
 	Name string `json:"name"`
 }
-
-// type StoreService struct {
-// 	DB *sql.DB
-// }
-
-// func (ss *StoreService) Create(name string) (*Store, error) {
-// 	store := Store{
-// 		Name: name,
-// 	}
-
-// 	row := ss.DB.QueryRow(`
-// 		INSERT INTO stores (name)
-// 		VALUES ($1) RETURNING id`,
-// 		name)
-
-// 	err := row.Scan(&store.ID)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("create store: %w", err)
-// 	}
-
-// 	log.Println("Store created!")
-// 	return &store, nil
-// }
